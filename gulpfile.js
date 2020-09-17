0340dd68e8e6563975e5662737c26f0fd31fff43
Elise Hoovler <elisejhoovler@live.com>
2020-09-17 13:45:26 -0500

    add pics

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,61 @@
-var gulp       = require('gulp');
-var babelify   = require('babelify');
-var browserify = require('browserify');
-var source     = require('vinyl-source-stream');
+"use strict";
 
-gulp.task('default', function() {
-    browserify('./modules/CartStarterKit.jsx.js')
-    .transform(babelify)
-    .bundle()
-    .pipe(source('CartStarterKit.js'))
-    .pipe(gulp.dest('./dist'));
-});
+// Load plugins
+const browsersync = require("browser-sync").create();
+const del = require("del");
+const gulp = require("gulp");
+const merge = require("merge-stream");
 
-gulp.task('examples', function() {
-    browserify('./examples/UnstyledCart.jsx.js').transform(babelify).bundle().pipe(source('UnstyledCart.js')).pipe(gulp.dest('./examples/build'));
-    browserify('./examples/BootstrapCart.jsx.js').transform(babelify).bundle().pipe(source('BootstrapCart.js')).pipe(gulp.dest('./examples/build'));
-    browserify('./examples/DragDropCart.jsx.js').transform(babelify).bundle().pipe(source('DragDropCart.js')).pipe(gulp.dest('./examples/build'));
-});
+// BrowserSync
+function browserSync(done) {
+  browsersync.init({
+    server: {
+      baseDir: "./"
+    },
+    port: 3000
+  });
+  done();
+}
+
+// BrowserSync reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
+
+// Clean vendor
+function clean() {
+  return del(["./vendor/"]);
+}
+
+// Bring third party dependencies from node_modules into vendor directory
+function modules() {
+  // Bootstrap
+  var bootstrap = gulp.src('./node_modules/bootstrap/dist/**/*')
+    .pipe(gulp.dest('./vendor/bootstrap'));
+  // jQuery
+  var jquery = gulp.src([
+      './node_modules/jquery/dist/*',
+      '!./node_modules/jquery/dist/core.js'
+    ])
+    .pipe(gulp.dest('./vendor/jquery'));
+  return merge(bootstrap, jquery);
+}
+
+// Watch files
+function watchFiles() {
+  gulp.watch("./**/*.css", browserSyncReload);
+  gulp.watch("./**/*.html", browserSyncReload);
+}
+
+// Define complex tasks
+const vendor = gulp.series(clean, modules);
+const build = gulp.series(vendor);
+const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
+
+// Export tasks
+exports.clean = clean;
+exports.vendor = vendor;
+exports.build = build;
+exports.watch = watch;
+exports.default = build;
